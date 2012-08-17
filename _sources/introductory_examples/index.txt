04f61adc14e1208644d886b8f7d4a4120a494277
Phil Elson <pelson.pub@gmail.com>
2012-08-17 14:55:53 +0100

    Initial cartopy docs. 0.0 pre-alpha.

---
+++ _sources/introductory_examples/index.txt
@@ -0,0 +1,9 @@
+Introductory examples using matplotlib
+======================================
+
+.. toctree::
+
+    01.great_circle.rst
+    02.polygon.rst
+    03.contours.rst
+    04.images.rst
---
+++ introductory_examples/03-contours-1.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+from cartopy.examples.waves import sample_data
+
+ax = plt.axes(projection=ccrs.Robinson())
+
+ax.set_global()
+
+lons, lats, data = sample_data(shape=(20, 40))
+
+plt.contourf(lons, lats, data, transform=ccrs.Geodetic())
+
+ax.coastlines()
+ax.gridlines()
+
+plt.show()
