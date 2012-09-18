516626946ebf79990bdba7f3cc20fa2f381089f2
Phil Elson <pelson.pub@gmail.com>
2012-09-18 17:37:07 +0100

    added copyright header.

--- lib/cartopy/io/srtm.py
+++ lib/cartopy/io/srtm.py
@@ -1,3 +1,21 @@
+# (C) British Crown Copyright 2011 - 2012, Met Office
+#
+# This file is part of cartopy.
+#
+# cartopy is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cartopy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with cartopy.  If not, see <http://www.gnu.org/licenses/>.
+
+
 """
 The Shuttle Radar Topography Mission (SRTM) is an international research effort that
 obtained digital elevation models on a near-global scale from 56S to 60N, to
