f0c5bb88da4b2f379aac45efeed6e4a0634894d1
Richard Hattersley <rhattersley@gmail.com>
2012-10-19 16:41:29 +0100

    Block non-projection transforms

---
+++ lib/cartopy/tests/mpl/test_axes.py
@@ -0,0 +1,53 @@
+# (C) British Crown Copyright 2011 - 2012, Met Office
+#
+# This file is part of cartopy.
+#
+# cartopy is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cartopy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with cartopy.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import cartopy.crs as ccrs
+
+
+class TestNoSpherical(unittest.TestCase):
+    def setUp(self):
+        self.ax = plt.axes(projection=ccrs.PlateCarree())
+        self.data = np.arange(12).reshape((3, 4))
+
+    def tearDown(self):
+        plt.clf()
+        plt.close()
+
+    def test_contour(self):
+        with self.assertRaises(ValueError):
+            self.ax.contour(self.data, transform=ccrs.Geodetic())
+
+    def test_contourf(self):
+        with self.assertRaises(ValueError):
+            self.ax.contourf(self.data, transform=ccrs.Geodetic())
+
+    def test_pcolor(self):
+        with self.assertRaises(ValueError):
+            self.ax.pcolor(self.data, transform=ccrs.Geodetic())
+
+    def test_pcolormesh(self):
+        with self.assertRaises(ValueError):
+            self.ax.pcolormesh(self.data, transform=ccrs.Geodetic())
+
+
+if __name__=='__main__':
+    unittest.main()
