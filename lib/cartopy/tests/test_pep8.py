77d47dbb7604087faacb4bb111603d2599ef4935
Phil Elson <pelson.pub@gmail.com>
2012-11-22 12:20:53 +0000

    Fixed failing pep8 tests and added the ability to add an 'exclude' file for unversioned files.

--- lib/cartopy/tests/test_pep8.py
+++ lib/cartopy/tests/test_pep8.py
@@ -9,10 +9,29 @@ import cartopy
 
 class TestCodeFormat(unittest.TestCase):
     def test_pep8_conformance(self):
+#
+#        Tests the cartopy codebase against the "pep8" tool.
+#
+#        Users can add their own excluded files (should files exist in the
+#        local directory which is not in the repository) by adding a
+#        ".pep8_test_exclude.txt" file in the same directory as this test.
+#        The file should be a line separated list of filenames/directories
+#        as can be passed to the "pep8" tool's exclude list.
+#
         pep8style = pep8.StyleGuide(quiet=False)
         pep8style.options.exclude.extend(['gshhs.py', 'examples'])
+
+        # allow users to add their own exclude list
+        extra_exclude_file = os.path.join(os.path.dirname(__file__),
+                                          '.pep8_test_exclude.txt')
+        if os.path.exists(extra_exclude_file):
+            with open(extra_exclude_file, 'r') as fh:
+                extra_exclude = [line.strip() for line in fh if line.strip()]
+            pep8style.options.exclude.extend(extra_exclude)
+
         result = pep8style.check_files([os.path.dirname(cartopy.__file__)])
-        self.assertEqual(result.total_errors, 0, "Found code syntax errors (and warnings).")
+        self.assertEqual(result.total_errors, 0, "Found code syntax "
+                                                 "errors (and warnings).")
 
 
 if __name__ == '__main__':
