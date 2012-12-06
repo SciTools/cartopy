38efc201e8cd8486f3ccd1576d4a5d4ae84184bf
Phil Elson <pelson.pub@gmail.com>
2012-12-06 11:57:01 +0000

    Tidied up the examples.

--- lib/cartopy/examples/waves.py
+++ lib/cartopy/examples/waves.py
@@ -5,30 +5,33 @@ import cartopy.crs as ccrs
 
 
 def sample_data(shape=(73, 145)):
-    """Returns lons, lats and data of some interesting data on a regular grid."""
-    nlats, nlons = shape 
-    lats = np.linspace(-np.pi/2, np.pi/2, nlats)
-    lons = np.linspace(0, 2*np.pi, nlons)
+    """Returns ``lons``, ``lats`` and ``data`` of some fake data."""
+    nlats, nlons = shape
+    lats = np.linspace(-np.pi / 2, np.pi / 2, nlats)
+    lons = np.linspace(0, 2 * np.pi, nlons)
     lons, lats = np.meshgrid(lons, lats)
-    wave = 0.75*(np.sin(2*lats)**8) * np.cos(4*lons)
-    mean = 0.5*np.cos(2*lats) * ((np.sin(2*lats))**2 + 2)
+    wave = 0.75 * (np.sin(2 * lats) ** 8) * np.cos(4 * lons)
+    mean = 0.5 * np.cos(2 * lats) * ((np.sin(2 * lats)) ** 2 + 2)
 
     lats = np.rad2deg(lats)
     lons = np.rad2deg(lons)
     data = wave + mean
-    
+
     return lons, lats, data
 
 
 def main():
-    # XXX This is really suffering from a lack of performance. Takes an inordinate
-    # (or is it infinite???) amount of time.
-    ax = plt.axes(projection=ccrs.NorthPolarStereo())
+    ax = plt.axes(projection=ccrs.Mollweide())
 
-    ax.contourf(*sample_data(), nlev=2, transform=ccrs.PlateCarree())
+    lons, lats, data = sample_data()
+
+    ax.contourf(lons, lats, data,
+                transform=ccrs.PlateCarree(),
+                cmap='spectral')
     ax.coastlines()
+    ax.set_global()
     plt.show()
-    
+
 
 if __name__ == '__main__':
     main()
