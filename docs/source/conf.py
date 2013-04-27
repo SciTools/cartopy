238d6447b301a36d4ce12f45ce06512134508350
pelson <pelson.pub@gmail.com>
2013-04-27 18:30:30 +0100

    Minor documentation template fixes.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -72,7 +72,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'cartopy'
-copyright = u'2011 - 2012 British Crown Copyright' # the template will need updating if this is changed
+copyright = u'2011 - 2013 British Crown Copyright' # the template will need updating if this is changed
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
