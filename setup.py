30eeefb5ef43ee5a6d38c388c4c4d787a00cb670
Phil Elson <pelson.pub@gmail.com>
2013-06-03 15:51:29 +0100

    Updated the version number to v0.8.0.

--- setup.py
+++ setup.py
@@ -127,7 +127,7 @@ class HeaderCheck(Command):
 
 setup(
     name='Cartopy',
-    version='0.8.x',
+    version='0.8.0',
     url='http://github.com/SciTools/cartopy',
     author='UK Met Office',
       
