71ea45efaa36dcd04b0efa0f13ee83f66e55a2a8
Andrew Dawson <ajdawson@acm.org>
2014-03-03 19:28:05 +0000

    Added a utility module containing a function to add a cyclic point.

---
+++ lib/cartopy/tests/test_util.py
@@ -0,0 +1,75 @@
+# (C) British Crown Copyright 2014, Met Office
+#
+# This file is part of cartopy.
+#
+# cartopy is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cartopy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with cartopy.  If not, see <http://www.gnu.org/licenses/>.
+from nose.tools import raises
+import numpy as np
+import numpy.ma as ma
+from numpy.testing import assert_array_equal
+
+from cartopy.util import add_cyclic_point
+
+
+class Test_add_cyclic_point(object):
+
+    @classmethod
+    def setup_class(cls):
+        cls.lons = np.arange(0, 360, 60)
+        cls.data2d = np.ones([3, 6]) * np.arange(6)
+        cls.data4d = np.ones([4, 6, 2, 3]) * \
+            np.arange(6)[..., np.newaxis, np.newaxis]
+
+    def test_data_only(self):
+        c_data = add_cyclic_point(self.data2d)
+        r_data = np.concatenate((self.data2d, self.data2d[:, :1]), axis=1)
+        assert_array_equal(c_data, r_data)
+
+    def test_data_and_coord(self):
+        c_data, c_lons = add_cyclic_point(self.data2d, coord=self.lons)
+        r_data = np.concatenate((self.data2d, self.data2d[:, :1]), axis=1)
+        r_lons = np.concatenate((self.lons, np.array([360.])))
+        assert_array_equal(c_data, r_data)
+        assert_array_equal(c_lons, r_lons)
+
+    def test_data_only_with_axis(self):
+        c_data = add_cyclic_point(self.data4d, axis=1)
+        r_data = np.concatenate((self.data4d, self.data4d[:, :1]), axis=1)
+        assert_array_equal(c_data, r_data)
+
+    def test_data_and_coord_with_axis(self):
+        c_data, c_lons = add_cyclic_point(self.data4d, coord=self.lons, axis=1)
+        r_data = np.concatenate((self.data4d, self.data4d[:, :1]), axis=1)
+        r_lons = np.concatenate((self.lons, np.array([360.])))
+        assert_array_equal(c_data, r_data)
+        assert_array_equal(c_lons, r_lons)
+
+    def test_masked_data(self):
+        new_data = ma.masked_less(self.data2d, 3)
+        c_data = add_cyclic_point(new_data)
+        r_data = ma.concatenate((self.data2d, self.data2d[:, :1]), axis=1)
+        assert_array_equal(c_data, r_data)
+
+    @raises(ValueError)
+    def test_invalid_coord_dimensionality(self):
+        lons2d = np.repeat(self.lons[np.newaxis], 3, axis=0)
+        c_data, c_lons = add_cyclic_point(self.data2d, coord=lons2d)
+
+    @raises(ValueError)
+    def test_invalid_coord_size(self):
+        c_data, c_lons = add_cyclic_point(self.data2d, coord=self.lons[:-1])
+
+    @raises(ValueError)
+    def test_invalid_axis(self):
+        c_data = add_cyclic_point(self.data2d, axis=-3)
