9fc84ee49df1d45ea26ed55d48c1741810f7fb7a
Phil Elson <pelson.pub@gmail.com>
2014-06-19 17:10:20 +0100

    Cartopy v0.11.0

--- lib/cartopy/__init__.py
+++ lib/cartopy/__init__.py
@@ -16,7 +16,7 @@
 # along with cartopy.  If not, see <http://www.gnu.org/licenses/>.
 
 
-__version__ = '0.11.x'
+__version__ = '0.11.0'
 __document_these__ = ['config']
 
 # Enable shapely performance enhancements
