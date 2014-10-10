902b0fbbb46a0aed8124b53dd9ee2daef4d81d0a
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2014-10-10 19:42:39 -0400

    Add coding standards test for __future__ imports.

--- lib/cartopy/tests/test_coding_standards.py
+++ lib/cartopy/tests/test_coding_standards.py
@@ -17,7 +17,10 @@
 
 from __future__ import (absolute_import, division, print_function)
 
+from fnmatch import fnmatch
+from itertools import chain
 import os
+import re
 import unittest
 
 import pep8
@@ -48,6 +51,48 @@ class TestCodeFormat(unittest.TestCase):
         result = pep8style.check_files([os.path.dirname(cartopy.__file__)])
         self.assertEqual(result.total_errors, 0, "Found code syntax "
                                                  "errors (and warnings).")
+
+
+class TestFutureImports(unittest.TestCase):
+    excluded = (
+        '*/cartopy/examples/*.py',
+        '*/docs/source/examples/*.py',
+    )
+
+    future_imports_pattern = re.compile(
+        r"^from __future__ import \(absolute_import,\s*division,\s*"
+        r"print_function(,\s*unicode_literals)?\)$",
+        flags=re.MULTILINE)
+
+    def test_future_imports(self):
+        # Tests that every single Python file includes the appropriate
+        # __future__ import to enforce consistent behaviour.
+        check_paths = [os.path.dirname(cartopy.__file__)]
+
+        failed = False
+        for dirpath, _, files in chain.from_iterable(os.walk(path)
+                                                     for path in check_paths):
+            for fname in files:
+                full_fname = os.path.join(dirpath, fname)
+                if not full_fname.endswith('.py'):
+                    continue
+                if not os.path.isfile(full_fname):
+                    continue
+                if any(fnmatch(full_fname, pat) for pat in self.excluded):
+                    continue
+
+                with open(full_fname, "r") as fh:
+                    content = fh.read()
+
+                    if re.search(self.future_imports_pattern, content) is None:
+                        print('The file {} has no valid __future__ imports '
+                              'and has not been excluded from the imports '
+                              'test.'.format(full_fname))
+                        failed = True
+
+        if failed:
+            raise ValueError('There were __future__ import check failures. '
+                             'See stdout.')
 
 
 if __name__ == '__main__':
