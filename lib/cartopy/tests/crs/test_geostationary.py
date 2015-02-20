5e3e63cd03ada70e5eb25abef156558bc5a541fe
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2015-02-20 04:24:09 -0500

    Fix description of Geostationary tests.

--- lib/cartopy/tests/crs/test_geostationary.py
+++ lib/cartopy/tests/crs/test_geostationary.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with cartopy.  If not, see <http://www.gnu.org/licenses/>.
 """
-Tests for the Transverse Mercator projection, including OSGB and OSNI.
+Tests for the Geostationary projection.
 
 """
 
