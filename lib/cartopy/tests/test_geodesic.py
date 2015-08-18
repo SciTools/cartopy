c80c289956b4da365beb720cbf42fb870d230e4f
scmc72 <scmc72@users.noreply.github.com>
2015-08-18 09:51:38 +0100

    Update test_geodesic.py

--- lib/cartopy/tests/test_geodesic.py
+++ lib/cartopy/tests/test_geodesic.py
@@ -25,25 +25,27 @@ from nose.tools import assert_equal
 
 from cartopy import geodesic
 
+
 class TestGeodesic(unittest.TestCase):
-    
+
     def test_dir(self):
         geod = geodesic.Geodesic()
         geod_dir = geod.direct([40, 50], 38, 2000)
-        assert_almost_equal(geod_dir, np.array([[40.01717933, 50.01416786, 
+        assert_almost_equal(geod_dir, np.array([[40.01717933, 50.01416786,
                             38.01316149]]), decimal=3)
-                            
+
     def test_inverse(self):
         geod = geodesic.Geodesic()
-        geod_inv = geod.inverse([40, 50], [45, 59])
+        geod_inv = geod.inverse([40, 50],
+                                [45, 59])
         assert_almost_equal(geod_inv, np.array([[1.05216248e+06, 1.59036831e+01,
                             1.99877316e+01]]), decimal=3)
 
     def test_circle(self):
         geod = geodesic.Geodesic()
-        geod_circle = geod.circle(40,50,500000,n_samples=3)
-        assert_almost_equal(geod_circle, np.array([[40., 54.49349757], 
-                            [34.23766162, 47.60355349], 
+        geod_circle = geod.circle(40, 50, 500000, n_samples=3)
+        assert_almost_equal(geod_circle, np.array([[40., 54.49349757],
+                            [34.23766162, 47.60355349],
                             [45.76233838, 47.60355349]]), decimal=3)
 
 if __name__ == '__main__':
