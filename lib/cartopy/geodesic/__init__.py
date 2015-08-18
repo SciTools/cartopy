988d074f1b15e72829e58956b917ebb71d7b96c0
scmc72 <scmc72@users.noreply.github.com>
2015-08-18 11:41:46 +0100

    Update __init__.py

--- lib/cartopy/geodesic/__init__.py
+++ lib/cartopy/geodesic/__init__.py
@@ -15,4 +15,5 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with cartopy.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import (absolute_import, division, print_function)
 from _geodesic import Geodesic
