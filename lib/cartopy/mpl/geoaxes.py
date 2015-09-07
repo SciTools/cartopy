7d8f3fae33301319ac95b3c8f844ac0aa031a0a8
daniel.attonbeckmann <beckmann@eld245.desktop.frd.metoffice.com>
2015-09-07 15:57:58 +0100

    fixed pep8 issues

--- lib/cartopy/mpl/geoaxes.py
+++ lib/cartopy/mpl/geoaxes.py
@@ -419,7 +419,7 @@ class GeoAxes(matplotlib.axes.Axes):
 
     def tissot(self, rad_km=5e5, n_samples=80, lon_n=10, lat_n=6, **kwargs):
         """
-        Adds Tissot's indicatrices to the current axes using the geodesic class.
+        Adds Tissot's indicatrices to the current axes with the geodesic class.
 
         Kwargs:
 
@@ -439,7 +439,7 @@ class GeoAxes(matplotlib.axes.Axes):
         for lat in np.linspace(-80, 80, lat_n):
             for lon in np.linspace(-180, 180, lon_n, endpoint=False):
                 circle = geod.circle(lon, lat, rad_km, n_samples=n_samples)
-                geoms.append(sgeom.Polygon(zip(circle[:,0],circle[:,1])))
+                geoms.append(sgeom.Polygon(zip(circle[:, 0], circle[:, 1])))
 
         feature = cartopy.feature.ShapelyFeature(geoms, ccrs.Geodetic(),
                                                  **kwargs)
