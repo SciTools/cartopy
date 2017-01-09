1b7649bbc3d3bdd2d67efcd6a6ea4c690598e39c
Corinne Bosley <corinne.bosley@metoffice.gov.uk>
2017-01-09 08:46:07 +0000

    new test file for transform_points added

--- lib/cartopy/tests/mpl/test_transform_points.py
+++ lib/cartopy/tests/mpl/test_transform_points.py
@@ -17,34 +17,46 @@
 
 from __future__ import (absolute_import, division, print_function)
 
-import matplotlib.pyplot as plt
-from matplotlib.testing.decorators import cleanup
-import numpy as np
-from numpy.testing import assert_array_almost_equal
-
 import cartopy.crs as ccrs
+import numpy as np
+import unittest
+from nose.tools import assert_true
+
 
 
 
 # TODO: Decide how I want to test this:
 # Graphics test? (Probably not).
 # Test for nans or infs in transformed arrays? (Maybe too contrived)
-# Some kind of test of the geoaxes (like in test_contour.py) - Check mpl for options
-def test_transform_to_orthographic():
+# Some kind of test of the geoaxes (like in test_contour.py) - Check mpl for options:
+# contains_point()?
+# get_visible()?
+# has_data()? (unlikely, but worth a try...)
 
+class TestTransformPoints(unittest.TestCase):
+    def __init__(self):
+        x = np.linspace(0, 360, 60).astype(np.float32)
+        y = np.linspace(-90, 90, 90).astype(np.float32)
 
+        self.x2d, self.y2d = np.meshgrid(x, y)
 
+        self.src_proj = ccrs.PlateCarree()
+    def test_transform_to_orthographic(self):
+        target_proj = ccrs.Orthographic()
+        proj_xyz = target_proj.transform_points(self.src_proj,
+                                                self.x2d, self.y2d)
+        assert_true(np.inf not in proj_xyz)
 
-def test_transform_to_transverse_mercator():
-
-
-
-
-def test_transform_to_gnomonic():
-
-
-
-def test_transform_to_geostationary():
+    # def test_transform_to_transverse_mercator():
+    #
+    #
+    #
+    #
+    # def test_transform_to_gnomonic():
+    #
+    #
+    #
+    # def test_transform_to_geostationary():
 
 
 
