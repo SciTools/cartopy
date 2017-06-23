d1c7f815dea529e4eb2a71f440a270683a814a57
Ryan May <rmay@ucar.edu>
2017-06-22 21:54:04 -0600

    ENH: Add the ability to easily grab a feature at a new scale

--- lib/cartopy/feature.py
+++ lib/cartopy/feature.py
@@ -187,6 +187,21 @@ class NaturalEarthFeature(Feature):
 
         return iter(geometries)
 
+    def with_scale(self, new_scale):
+        """
+        Return a copy of the feature with a new scale.
+
+        Args:
+
+        * new_scale:
+            The new dataset scale, i.e. one of '10m', '50m', or '110m'.
+            Corresponding to 1:10,000,000, 1:50,000,000, and 1:110,000,000
+            respectively.
+
+        """
+        return NaturalEarthFeature(self.category, self.name, new_scale,
+                                   **self.kwargs)
+
 
 class GSHHSFeature(Feature):
     """
