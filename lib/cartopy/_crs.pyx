ce0dc08a8397a3c6f7d45ce5ec06ee5524e0bc47
Daniel Kirkham <daniel.kirkham@metoffice.gov.uk>
2017-09-29 14:46:00 +0100

    Include NULL checks

--- lib/cartopy/_crs.pyx
+++ lib/cartopy/_crs.pyx
@@ -135,7 +135,8 @@ cdef class CRS:
         self.proj4 = NULL
 
     def __dealloc__(self):
-        pj_free(self.proj4)
+        if self.proj4 is not NULL:
+            pj_free(self.proj4)
 
     def __init__(self, proj4_params, globe=None):
         """
@@ -168,8 +169,9 @@ cdef class CRS:
                 init_items.append('+{}'.format(k))
         self.proj4_init = ' '.join(init_items) + ' +no_defs'
         proj4_init_bytes = six.b(self.proj4_init)
-        if self.proj4 is NULL:
-            self.proj4 = pj_init_plus(proj4_init_bytes)
+        if self.proj4 is not NULL:
+            pj_free(self.proj4)
+        self.proj4 = pj_init_plus(proj4_init_bytes)
         if not self.proj4:
             raise Proj4Error()
 
