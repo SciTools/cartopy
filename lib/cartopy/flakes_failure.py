9842a43b2346e765791758ca92843a2a3b0ff927
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2017-11-27 16:18:00 +0000

    Add badly-styled sourcefile.

---
+++ lib/cartopy/flakes_failure.py
@@ -0,0 +1,8 @@
+import os as unused
+
+def BadFunction(BadVARNAME):
+   # odd indent
+   # very long line very long line very long line very long line very long line very long line
+   return 1
+
+# extra line
