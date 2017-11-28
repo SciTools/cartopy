53f3761a5fcece39f05aa9dc7b6d525904ea4499
Corinne Bosley <corinne.e.bosley@gmail.com>
2017-11-28 14:51:41 +0000

    licence header

--- docs/add_noshow.py
+++ docs/add_noshow.py
@@ -1,3 +1,20 @@
+# (C) British Crown Copyright 2011 - 2017, Met Office
+#
+# This file is part of cartopy.
+#
+# cartopy is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cartopy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with cartopy.  If not, see <https://www.gnu.org/licenses/>.
+
 from bs4 import BeautifulSoup as bs
 
 path_html = 'build/html/gallery/index.html'
