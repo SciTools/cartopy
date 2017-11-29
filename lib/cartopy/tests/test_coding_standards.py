09cc6d1aa75be0af00a844601cb148cccf8cec4d
Corinne Bosley <corinne.e.bosley@gmail.com>
2017-11-29 12:35:04 +0000

    removed testing class TestCodeFormat as well

--- lib/cartopy/tests/test_coding_standards.py
+++ lib/cartopy/tests/test_coding_standards.py
@@ -161,35 +161,6 @@ class TestLicenseHeaders(object):
             raise ValueError('There were license header failures. See stdout.')
 
 
-class TestCodeFormat(object):
-    def test_pep8_conformance(self):
-        # Tests the cartopy codebase against the "pep8" tool.
-        #
-        # Users can add their own excluded files (should files exist in the
-        # local directory which is not in the repository) by adding a
-        # ".pep8_test_exclude.txt" file in the same directory as this test.
-        # The file should be a line separated list of filenames/directories
-        # as can be passed to the "pep8" tool's exclude list.
-        pep8style = pep8.StyleGuide(quiet=False)
-        pep8style.options.exclude.extend(['trace.py', '_crs.py',
-                                          '*/cartopy/geodesic/_geodesic.py'])
-
-        # Ignore E402 module level import not at top of file
-        pep8style.options.ignore += ('E402', )
-
-        # allow users to add their own exclude list
-        extra_exclude_file = os.path.join(os.path.dirname(__file__),
-                                          '.pep8_test_exclude.txt')
-        if os.path.exists(extra_exclude_file):
-            with open(extra_exclude_file, 'r') as fh:
-                extra_exclude = [line.strip() for line in fh if line.strip()]
-            pep8style.options.exclude.extend(extra_exclude)
-
-        result = pep8style.check_files([CARTOPY_DIR])
-        assert result.total_errors == 0, \
-            "Found code syntax errors (and warnings)."
-
-
 class TestFutureImports(object):
     excluded = (
         '*/cartopy/examples/*.py',
