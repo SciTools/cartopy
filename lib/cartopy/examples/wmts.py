03e10b7f8e7a772317a4352d20984212e61910be
raybellwaves <rjbell1987@gmail.com>
2017-12-01 22:51:01 -0500

    Added projection type to .set_extent

--- lib/cartopy/examples/wmts.py
+++ lib/cartopy/examples/wmts.py
@@ -16,8 +16,8 @@ and Atmospheric Administration (NOAA).
 
 """
 
-import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
 
 
 def main():
@@ -27,7 +27,7 @@ def main():
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
     ax.add_wmts(url, layer)
-    ax.set_extent((-15, 25, 35, 60))
+    ax.set_extent([-15, 25, 35, 60], crs=ccrs.PlateCarree())
 
     ax.set_title('Suomi NPP Earth at night April/October 2012')
     plt.show()
