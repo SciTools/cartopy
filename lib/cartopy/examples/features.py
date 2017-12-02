d8516843a6247996b8579cf521d3616954c284f7
raybellwaves <rjbell1987@gmail.com>
2017-12-01 22:20:57 -0500

    added import cartopy.feature as cfeature to match feature_creation.py

--- lib/cartopy/examples/features.py
+++ lib/cartopy/examples/features.py
@@ -1,21 +1,20 @@
 __tags__ = ['Lines and polygons']
-import cartopy
-import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
 
 
 def main():
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
-
-    ax.add_feature(cartopy.feature.LAND)
-    ax.add_feature(cartopy.feature.OCEAN)
-    ax.add_feature(cartopy.feature.COASTLINE)
-    ax.add_feature(cartopy.feature.BORDERS, linestyle=':')
-    ax.add_feature(cartopy.feature.LAKES, alpha=0.5)
-    ax.add_feature(cartopy.feature.RIVERS)
-
     ax.set_extent([-20, 60, -40, 45], crs=ccrs.PlateCarree())
+
+    ax.add_feature(cfeature.LAND)
+    ax.add_feature(cfeature.OCEAN)
+    ax.add_feature(cfeature.COASTLINE)
+    ax.add_feature(cfeature.BORDERS, linestyle=':')
+    ax.add_feature(cfeature.LAKES, alpha=0.5)
+    ax.add_feature(cfeature.RIVERS)
 
     plt.show()
 
