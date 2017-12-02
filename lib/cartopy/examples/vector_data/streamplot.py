b7b307f898794e0519b3ca2798b237a9d765dd1f
raybellwaves <rjbell1987@gmail.com>
2017-12-01 22:45:57 -0500

    Changed domain and added projection type to .set_extent

--- lib/cartopy/examples/vector_data/streamplot.py
+++ lib/cartopy/examples/vector_data/streamplot.py
@@ -15,7 +15,7 @@ from cartopy.examples.vector_data.arrows import sample_data
 def main():
     fig = plt.figure(figsize=(10, 5))
     ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
-    ax.set_extent([-90, 75, 10, 60])
+    ax.set_extent([-90, 75, 10, 85], crs=ccrs.PlateCarree())
     ax.coastlines()
 
     x, y, u, v, vector_crs = sample_data(shape=(80, 100))
