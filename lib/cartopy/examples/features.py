1fec0417250148069b24b83f5037d68d820f8f4b
raybellwaves <rjbell1987@gmail.com>
2017-12-01 22:14:34 -0500

    Added import cartopy.crs as ccrs. Changed domain projection type to .set_extent

--- lib/cartopy/examples/features.py
+++ lib/cartopy/examples/features.py
@@ -1,11 +1,12 @@
 __tags__ = ['Lines and polygons']
 import cartopy
+import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 
 
 def main():
     fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1, projection=cartopy.crs.PlateCarree())
+    ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
 
     ax.add_feature(cartopy.feature.LAND)
     ax.add_feature(cartopy.feature.OCEAN)
@@ -14,7 +15,7 @@ def main():
     ax.add_feature(cartopy.feature.LAKES, alpha=0.5)
     ax.add_feature(cartopy.feature.RIVERS)
 
-    ax.set_extent([-20, 60, -40, 40])
+    ax.set_extent([-20, 60, -40, 45], crs=ccrs.PlateCarree())
 
     plt.show()
 
