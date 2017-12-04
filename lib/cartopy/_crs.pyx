eeae19f14da5b299f6d91b4762ac1ac9eeef4f59
Daniel Kirkham <daniel.kirkham@metoffice.gov.uk>
2017-12-04 13:12:03 +0000

    Replace 'is not' with '!='

--- lib/cartopy/_crs.pyx
+++ lib/cartopy/_crs.pyx
@@ -135,7 +135,7 @@ cdef class CRS:
         self.proj4 = NULL
 
     def __dealloc__(self):
-        if self.proj4 is not NULL:
+        if self.proj4 != NULL:
             pj_free(self.proj4)
 
     def __init__(self, proj4_params, globe=None):
@@ -169,7 +169,7 @@ cdef class CRS:
                 init_items.append('+{}'.format(k))
         self.proj4_init = ' '.join(init_items) + ' +no_defs'
         proj4_init_bytes = six.b(self.proj4_init)
-        if self.proj4 is not NULL:
+        if self.proj4 != NULL:
             pj_free(self.proj4)
         self.proj4 = pj_init_plus(proj4_init_bytes)
         if not self.proj4:
