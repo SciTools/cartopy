bf3e79e3ec56209c140e91ecdb8caabbb28218e3
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2018-01-04 14:49:45 +0000

    Made gridlines inline code into an example.

---
+++ lib/cartopy/examples/miscellanea/gridline_features.py
@@ -0,0 +1,36 @@
+"""
+Gridline Features
+-----------------
+
+This example makes use of many of the features of the
+:class:`~cartopy.mpl.Gridliner` class to produce customized gridlines
+and tick labels.
+
+"""
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+
+import cartopy.crs as ccrs
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+
+def main():
+    ax = plt.axes(projection=ccrs.Mercator())
+    ax.coastlines()
+
+    gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True, 
+                      linewidth=2, color='gray', alpha=0.5, linestyle='--')
+
+    gl.xlabels_top = False
+    gl.ylabels_left = False
+    gl.xlines = False
+    gl.xlocator = mticker.FixedLocator([-180, -45, 0, 45, 180])
+    gl.xformatter = LONGITUDE_FORMATTER
+    gl.yformatter = LATITUDE_FORMATTER
+    gl.xlabel_style = {'size': 15, 'color': 'gray'}
+    gl.xlabel_style = {'color': 'red', 'weight': 'bold'}
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
