f8f6d62a86ae2357d86117e930d21fbd550d2bb5
Corinne Bosley <corinne.e.bosley@gmail.com>
2018-01-05 16:01:27 +0000

    license headers

--- lib/cartopy/geodesic/_geodesic.pyx
+++ lib/cartopy/geodesic/_geodesic.pyx
@@ -1,4 +1,4 @@
-# (C) British Crown Copyright 2015 - 2017, Met Office
+# (C) British Crown Copyright 2015 - 2018, Met Office
 #
 # This file is part of cartopy.
 #
--- lib/cartopy/mpl/feature_artist.py
+++ lib/cartopy/mpl/feature_artist.py
@@ -1,4 +1,4 @@
-# (C) British Crown Copyright 2011 - 2017, Met Office
+# (C) British Crown Copyright 2011 - 2018, Met Office
 #
 # This file is part of cartopy.
 #
