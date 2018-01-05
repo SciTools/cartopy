31c2d350c82bf59abdca795b1aa47a1fd96e9909
Corinne Bosley <corinne.e.bosley@gmail.com>
2018-01-05 11:40:26 +0000

    docstring corrections to cartopy.vector_transform.py

--- lib/cartopy/vector_transform.py
+++ lib/cartopy/vector_transform.py
@@ -31,9 +31,23 @@ def _interpolate_to_grid(nx, ny, x, y, *scalars, **kwargs):
     Interpolates two vector components and zero or more scalar fields,
     which can be irregular, to a regular grid.
 
-    Kwargs:
+    Parameters
+    ----------
+    nx
+        Number of points at which to interpolate in x direction.
+    ny
+        Number of points at which to interpolate in y direction.
+    x
+        Array of source points in x direction.
+    y
+        Array of source points in y direction.
 
-    * target_extent:
+    Other Parameters
+    ----------------
+    scalars
+        Zero or more scalar fields to regrid along with the vector
+        components.
+    target_extent
         The extent in the target CRS that the grid should occupy, in the
         form ``(x-lower, x-upper, y-lower, y-upper)``. Defaults to cover
         the full extent of the vector field.
@@ -56,56 +70,49 @@ def _interpolate_to_grid(nx, ny, x, y, *scalars, **kwargs):
 def vector_scalar_to_grid(src_crs, target_proj, regrid_shape, x, y, u, v,
                           *scalars, **kwargs):
     """
-    Transform and interpolate a vector field to a regular grid in the
+    Transforms and interpolates a vector field to a regular grid in the
     target projection.
 
-    Args:
-
-    * src_crs:
+    Parameters
+    ----------
+    src_crs
         The :class:`~cartopy.crs.CRS` that represents the coordinate
         system the vectors are defined in.
-
-    * target_proj:
+    target_proj
         The :class:`~cartopy.crs.Projection` that represents the
         projection the vectors are to be transformed to.
-
-    * regrid_shape:
+    regrid_shape
         The regular grid dimensions. If a single integer then the grid
         will have that number of points in the x and y directions. A
         2-tuple of integers specify the size of the regular grid in the
         x and y directions respectively.
-
-    * x, y:
+    x, y
         The x and y coordinates, in the source CRS coordinates,
         where the vector components are located.
-
-    * u, v:
+    u, v
         The grid eastward and grid northward components of the
         vector field respectively. Their shapes must match.
 
-    * scalars:
+    Other Parameters
+    ----------------
+    scalars
         Zero or more scalar fields to regrid along with the vector
         components. Each scalar field must have the same shape as the
         vector components.
-
-    Kwargs:
-
-    * target_extent:
+    target_extent
         The extent in the target CRS that the grid should occupy, in the
         form ``(x-lower, x-upper, y-lower, y-upper)``. Defaults to cover
         the full extent of the vector field.
 
-    Returns:
-
-    * x_grid, y_grid:
+    Returns
+    -------
+    x_grid, y_grid
         The x and y coordinates of the regular grid points as
         2-dimensional arrays.
-
-    * u_grid, v_grid:
+    u_grid, v_grid
         The eastward and northward components of the vector field on
         the regular grid.
-
-    * scalars_grid:
+    scalars_grid
         The scalar fields on the regular grid. The number of returned
         scalar fields is the same as the number that were passed in.
 
