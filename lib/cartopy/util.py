808887c9ac1fe050fcf343e943a035335113f164
Corinne Bosley <corinne.e.bosley@gmail.com>
2018-01-08 11:47:55 +0000

    possible fix for mysterious and suspicious doctest failure

--- lib/cartopy/util.py
+++ lib/cartopy/util.py
@@ -60,22 +60,22 @@ def add_cyclic_point(data, coord=None, axis=-1):
     >>> data = np.ones([5, 6]) * np.arange(6)
     >>> cyclic_data = add_cyclic_point(data)
     >>> print(cyclic_data)
-    [[ 0.  1.  2.  3.  4.  5.  0.]
-     [ 0.  1.  2.  3.  4.  5.  0.]
-     [ 0.  1.  2.  3.  4.  5.  0.]
-     [ 0.  1.  2.  3.  4.  5.  0.]
-     [ 0.  1.  2.  3.  4.  5.  0.]]
+    [[ 0. 1. 2. 3. 4. 5. 0.]
+     [ 0. 1. 2. 3. 4. 5. 0.]
+     [ 0. 1. 2. 3. 4. 5. 0.]
+     [ 0. 1. 2. 3. 4. 5. 0.]
+     [ 0. 1. 2. 3. 4. 5. 0.]]
 
     Adding a cyclic point to a data array and an associated coordinate
 
     >>> lons = np.arange(0, 360, 60)
     >>> cyclic_data, cyclic_lons = add_cyclic_point(data, coord=lons)
     >>> print(cyclic_data)
-    [[ 0.  1.  2.  3.  4.  5.  0.]
-     [ 0.  1.  2.  3.  4.  5.  0.]
-     [ 0.  1.  2.  3.  4.  5.  0.]
-     [ 0.  1.  2.  3.  4.  5.  0.]
-     [ 0.  1.  2.  3.  4.  5.  0.]]
+    [[ 0. 1. 2. 3. 4. 5. 0.]
+     [ 0. 1. 2. 3. 4. 5. 0.]
+     [ 0. 1. 2. 3. 4. 5. 0.]
+     [ 0. 1. 2. 3. 4. 5. 0.]
+     [ 0. 1. 2. 3. 4. 5. 0.]]
     >>> print(cyclic_lons)
     [  0  60 120 180 240 300 360]
 
