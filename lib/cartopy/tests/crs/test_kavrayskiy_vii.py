72f5d8abb250a7922ed4f548e39058fb6c29135d
pierre-prandi <prandi.pierre@gmail.com>
2018-05-23 14:02:47 +0200

    basic testing for kav7 projection

---
+++ lib/cartopy/tests/crs/test_kavrayskiy_vii.py
@@ -0,0 +1,28 @@
+# (C) British Crown Copyright 2015 - 2017, Met Office
+#
+# This file is part of cartopy.
+#
+# cartopy is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cartopy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with cartopy.  If not, see <https://www.gnu.org/licenses/>.
+
+from __future__ import (absolute_import, division, print_function)
+import cartopy.crs as ccrs
+
+
+class TestKavrayskiyVII(object):
+
+    def test_default(self):
+        ''' testing that the proj4 params are right '''
+        crs = ccrs.KavrayskiyVII()
+        expected = ('+ellps=WGS84 +proj=kav7 +lon_0=0 +no_defs)
+        assert crs.proj4_init == expected
