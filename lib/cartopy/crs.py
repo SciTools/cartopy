e53056606e0c7abc0f72367f053d096bc69e46cd
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2018-09-27 19:12:20 -0400

    Buffer exterior rings when creating polygons.
    This is already done for interior rings, and prevents invalid geometries
    from crashing things later.

--- lib/cartopy/crs.py
+++ lib/cartopy/crs.py
@@ -545,7 +545,8 @@ class Projection(six.with_metaclass(ABCMeta, CRS)):
         # Turn all the exterior rings into polygon definitions,
         # "slurping up" any interior rings they contain.
         for exterior_ring in exterior_rings:
-            polygon = sgeom.Polygon(exterior_ring)
+            # Use shapely buffer in an attempt to fix invalid geometries
+            polygon = sgeom.Polygon(exterior_ring).buffer(0)
             prep_polygon = prep(polygon)
             holes = []
             for interior_ring in interior_rings[:]:
