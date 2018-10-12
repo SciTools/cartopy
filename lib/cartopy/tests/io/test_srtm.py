fd31081c5acc20768b13bc1fa59b895a67206453
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2018-10-12 16:42:50 -0400

    Fix bugs in SRTM tests.

--- lib/cartopy/tests/io/test_srtm.py
+++ lib/cartopy/tests/io/test_srtm.py
@@ -26,6 +26,8 @@ import pytest
 import cartopy.crs as ccrs
 import cartopy.io.srtm
 
+from .test_downloaders import download_to_temp  # noqa: F401 (used as fixture)
+
 
 pytestmark = [pytest.mark.skip('SRTM login not supported'),
               pytest.mark.network]
@@ -89,8 +91,10 @@ class TestSRTMSource__single_tile(object):
     def test_in_range(self, Source):
         if Source == cartopy.io.srtm.SRTM3Source:
             shape = (1201, 1201)
-        elif Source == cartopy.io.srtm.SRTM3Source:
+        elif Source == cartopy.io.srtm.SRTM1Source:
             shape = (3601, 3601)
+        else:
+            raise ValueError('Source is of unexpected type.')
         source = Source()
         img, crs, extent = source.single_tile(-1, 50)
         assert isinstance(img, np.ndarray)
