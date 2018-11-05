55d7a371c1e05cbebcfca26d98fab58fa4aabcf4
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2018-11-05 01:40:59 -0500

    DOC: Add NumPy to intersphinx mapping.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -347,6 +347,7 @@ epub_copyright = u'2012, Philip Elson, Richard Hattersley'
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'python': ('https://docs.python.org/3', None),
                        'matplotlib': ('https://matplotlib.org', None),
+                       'numpy': ('https://docs.scipy.org/doc/numpy/', None),
                        'shapely': ('http://toblerity.org/shapely', None), }
 
 
