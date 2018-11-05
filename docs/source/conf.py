c86b26ca9d51295b21efcbce93a0740657e47881
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2018-11-05 01:39:30 -0500

    DOC: Set default role to py:obj.
    These means we can just use backticks and don't need to add a specific
    :func:, :class:, etc.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -124,7 +124,7 @@ sphinx_gallery_conf = {
 exclude_patterns = []
 
 # The reST default role (used for this markup: `text`) to use for all documents.
-# default_role = None
+default_role = 'py:obj'
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 # add_function_parentheses = True
