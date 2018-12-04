d90b8b84e2b3e704724368fc5c399c8f2ae7ee25
Phil Elson <pelson.pub@gmail.com>
2018-12-03 16:28:52 -1000

    Made image tile fetching multi-threaded. Constant size of 24 threads defined, but can be monkey-patched on the GoogleWTS class if necessary.

--- lib/cartopy/io/img_tiles.py
+++ lib/cartopy/io/img_tiles.py
@@ -33,6 +33,7 @@ from __future__ import (absolute_import, division, print_function)
 from abc import ABCMeta, abstractmethod
 import warnings
 
+import concurrent.futures
 from PIL import Image
 import shapely.geometry as sgeom
 import numpy as np
@@ -48,6 +49,8 @@ class GoogleWTS(six.with_metaclass(ABCMeta, object)):
     A "tile" in this class refers to the coordinates (x, y, z).
 
     """
+    _MAX_THREADS = 24
+
     def __init__(self, desired_tile_form='RGB'):
         self.imgs = []
         self.crs = ccrs.Mercator.GOOGLE
@@ -55,15 +58,30 @@ class GoogleWTS(six.with_metaclass(ABCMeta, object)):
 
     def image_for_domain(self, target_domain, target_z):
         tiles = []
-        for tile in self.find_images(target_domain, target_z):
+
+        def fetch_tile(tile):
             try:
                 img, extent, origin = self.get_image(tile)
             except IOError:
-                continue
+                # Some services 404 for tiles that aren't supposed to be
+                # there (e.g. out of range).
+                raise
             img = np.array(img)
             x = np.linspace(extent[0], extent[1], img.shape[1])
             y = np.linspace(extent[2], extent[3], img.shape[0])
-            tiles.append([img, x, y, origin])
+            return img, x, y, origin
+
+        with concurrent.futures.ThreadPoolExecutor(
+                max_workers=self._MAX_THREADS) as executor:
+            futures = []
+            for tile in self.find_images(target_domain, target_z):
+                futures.append(executor.submit(fetch_tile, tile))
+            for future in concurrent.futures.as_completed(futures):
+                try:
+                    img, x, y, origin = future.result()
+                    tiles.append([img, x, y, origin])
+                except IOError:
+                    pass
 
         img, extent, origin = _merge_tiles(tiles)
         return img, extent, origin
