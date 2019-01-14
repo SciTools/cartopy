Fix precision issue with ticker

lib/cartopy/mpl/ticker.py
@@ -127,11 +127,12 @@ class _PlateCarreeFormatter(Formatter):
         mins: minutes
         secs: seconds
         """
-        x = np.round(np.asarray(x, 'd'), self._precision)
+        self._precision = 6
+        x = np.asarray(x, 'd')
         degs = np.round(x, self._precision).astype('i')
         y = (x - degs) * 60
         mins = np.round(y, self._precision).astype('i')
-        secs = np.round((y - mins) * 60, self._precision - 3)
+        secs = np.round((y - mins)*60, self._precision - 3)
         return x, degs, mins, secs
 
     def set_locs(self, locs):

lib/cartopy/tests/mpl/test_ticker.py
@@ -270,6 +270,6 @@ def test_LatitudeFormatter_minutes_seconds(test_ticks, expected):
                                        id='lon_tiny'),
                           ])
 def test_LongitudeLocator(cls, vmin, vmax, expected):
-    locator = cls()
+    locator = cls(decimal=False)
     result = locator.tick_values(vmin, vmax)
     np.testing.assert_allclose(result, expected)