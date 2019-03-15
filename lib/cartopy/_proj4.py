64cb8ce7d58c996575ac06f8b6eb173fc070109a
David Hoese <david.hoese@ssec.wisc.edu>
2019-03-15 09:53:39 -0500

    Fix proj threshold if x/y bounds are flipped

--- lib/cartopy/_proj4.py
+++ lib/cartopy/_proj4.py
@@ -121,7 +121,7 @@ class _PROJ4Projection(ccrs.Projection):
     @property
     def threshold(self):
         x0, x1, y0, y1 = self.bounds
-        return min(x1 - x0, y1 - y0) / 100.
+        return min(abs(x1 - x0), abs(y1 - y0)) / 100.
 
 
 def _all_subclasses(cls):
