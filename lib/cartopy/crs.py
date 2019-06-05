3231ebc53605db474665a8873e8f40be32ab2b8b
stephenworsley <49274989+stephenworsley@users.noreply.github.com>
2019-06-05 16:04:12 +0100

    Remove Whitespace

--- lib/cartopy/crs.py
+++ lib/cartopy/crs.py
@@ -549,7 +549,7 @@ class Projection(six.with_metaclass(ABCMeta, CRS)):
                 return coords[0] != coords[-1]
             else:
                 return len(linear_ring.coords) > 3 and linear_ring.is_valid
-        
+
         linear_rings = [
             sgeom.LinearRing(linear_ring)
             for linear_ring in processed_ls
