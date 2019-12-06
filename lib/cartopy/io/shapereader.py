0c825fe1072576a1e96c3a3a28809e5b1f3c9b81
shevawen <sheva.wen@gmail.com>
2019-12-07 07:12:28 +0800

    Bugfix for shape file reading
    Some of the shapefiles have shapes that can not be represented as geometry, for example, Natural Earth Rivers-Lake-Centerlines shapefiles after version 4.0.0.
    
    ```python
    import shapefile
    
    sf = shapefile.Reader("ne_50m_rivers_lake_centerlines.shp")
    assert(0 == sf.shapes()[-2].shapeType)
    ```
    
    This shape can cause an exception when the feature is added to axes.
    ```python
    import cartopy
    import cartopy.crs as ccrs
    import cartopy.feature as cfeat
    
    proj = ccrs.PlateCarree()  
    fig = plt.figure(figsize=(9, 8))
    
    ax = fig.subplots(1, 1, subplot_kw={'projection': proj})
    ax.add_feature(cfeat.RIVERS.with_scale('50m'))
    ```

--- lib/cartopy/io/shapereader.py
+++ lib/cartopy/io/shapereader.py
@@ -163,7 +163,10 @@ class BasicReader(object):
 
         """
         for i in range(self._reader.numRecords):
-            yield sgeom.shape(self._reader.shape(i))
+            shape = self._reader.shape(i)
+            # Skip the shape that can not be represented as geometry.
+            if shape.shapeType != shapefile.NULL:
+                yield sgeom.shape(shape)
 
     def records(self):
         """
