89f4edde725ae9c936a06d5950da06c9d6cad03b
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2020-02-08 03:23:35 -0500

    Revert changes to licensing files.
    The `COPYING.LESSER` that was moved to `LICENSE` only contains the
    *additional* terms of the LGPL. The `COPYING` file is still required, as
    it contains the full terms of the GPL which the LGPL extends.

--- lib/cartopy/__init__.py
+++ lib/cartopy/__init__.py
@@ -1,7 +1,8 @@
 # Copyright Cartopy Contributors
 #
 # This file is part of Cartopy and is released under the LGPL license.
-# See LICENSE in the root of the repository for full licensing details.
+# See COPYING and COPYING.LESSER in the root of the repository for full
+# licensing details.
 
 from __future__ import (absolute_import, division, print_function)
 
