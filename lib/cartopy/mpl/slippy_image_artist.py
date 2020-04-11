3c24b06deda8c9e618dd2809fea4e2748982b1cf
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2020-04-11 04:32:43 -0400

    Don't allow SlippyImageArtist to affect Figure layout.
    This is slightly more correct than the previous commit, but only
    supported in newer Matplotlib.

--- lib/cartopy/mpl/slippy_image_artist.py
+++ lib/cartopy/mpl/slippy_image_artist.py
@@ -39,6 +39,9 @@ class SlippyImageArtist(AxesImage):
     """
     def __init__(self, ax, raster_source, **kwargs):
         self.raster_source = raster_source
+        if matplotlib.__version__ >= '3':
+            # This artist fills the Axes, so should not influence layout.
+            kwargs.setdefault('in_layout', False)
         super(SlippyImageArtist, self).__init__(ax, **kwargs)
         self.cache = []
 
