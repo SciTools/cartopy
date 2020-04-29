fa6212b1d463e16fb24a7b66c60d58614e8f093c
Hugo <hugovk@users.noreply.github.com>
2020-04-29 09:13:07 +0300

    The future is now

--- lib/cartopy/tests/test_coding_standards.py
+++ lib/cartopy/tests/test_coding_standards.py
@@ -7,8 +7,6 @@
 
 from datetime import datetime
 from fnmatch import fnmatch
-import io
-from itertools import chain
 import os
 import re
 import subprocess
@@ -102,56 +100,3 @@ class TestLicenseHeaders:
                     failed.append(full_fname)
 
         assert failed == [], 'There were license header failures.'
-
-
-class TestFutureImports:
-    excluded = (
-        '*/cartopy/examples/*.py',
-        '*/docs/source/examples/*.py',
-        '*/cartopy/_crs.py',   # A file created by setuptools for so loading.
-        '*/cartopy/trace.py',  # Ditto.
-        '*/cartopy/geodesic/_geodesic.py',
-        '*/cartopy/_version.py',
-    )
-
-    future_imports_pattern = re.compile(
-        r"^from __future__ import \(absolute_import,\s*division,\s*"
-        r"print_function(,\s*unicode_literals)?\)$",
-        flags=re.MULTILINE)
-
-    def test_future_imports(self):
-        # Tests that every single Python file includes the appropriate
-        # __future__ import to enforce consistent behaviour.
-        check_paths = [CARTOPY_DIR]
-
-        failed = False
-        for dirpath, _, files in chain.from_iterable(os.walk(path)
-                                                     for path in check_paths):
-            for fname in files:
-                full_fname = os.path.join(dirpath, fname)
-                if not full_fname.endswith('.py'):
-                    continue
-                if not os.path.isfile(full_fname):
-                    continue
-                if any(fnmatch(full_fname, pat) for pat in self.excluded):
-                    continue
-
-                is_empty = os.path.getsize(full_fname) == 0
-
-                with open(full_fname, "r", encoding='utf-8') as fh:
-                    content = fh.read()
-
-                has_future_import = re.search(
-                    self.future_imports_pattern, content) is not None
-
-                if is_empty:
-                    pass
-                elif not has_future_import:
-                    print('The file {} has no valid __future__ imports '
-                          'and has not been excluded from the imports '
-                          'test.'.format(full_fname))
-                    failed = True
-
-        if failed:
-            raise ValueError('There were __future__ import check failures. '
-                             'See stdout.')
