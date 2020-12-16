3f3b2314a690006e41467b0da795b9641dbd1022
Philipe_Leal <leal.philipe@gmail.com>
2020-12-16 16:45:18 -0300

    test suit for scalebar has been updated

--- lib/cartopy/tests/mpl/test_scalebar.py
+++ lib/cartopy/tests/mpl/test_scalebar.py
@@ -4,13 +4,7 @@
 # See COPYING and COPYING.LESSER in the root of the repository for full
 # licensing details.
 
-from unittest import mock
-
-from matplotlib.testing.decorators import cleanup
-import matplotlib.path as mpath
 import matplotlib.pyplot as plt
-import numpy as np
-import pytest
 
 import cartopy.crs as ccrs
 
@@ -23,21 +17,20 @@ def test_scalebar():
     # sorted data to disappear
 
     fig, axes = plt.subplots(1, 2, subplot_kw={'projection': ccrs.Mercator()})
-    
-    projections = [ccrs.Mercator(), ccrs.PlateCarree()]
-    
-    axes = axes.ravel()
-    
-    for proj,  ax in zip(projections, axes):
-        ax.projection = projection
-        fancy_scalebar(ax, 
-                       location=(0.5, 0.2), 
-                       length=10000,
-                       metres_per_unit=1000, 
-                       unit_name='km',
-                       tol=0.01, 
-                           angle=0,
-                           max_stripes=5,
-                           fontsize= 8,
-                           dy = 0.05)
 
+    projections = [ccrs.Mercator(), ccrs.PlateCarree()]
+
+    axes = axes.ravel()
+
+    for proj, ax in zip(projections, axes):
+        ax.projection = proj
+        fancy_scalebar(ax,
+                       location=(0.5, 0.2),
+                       length=10000,
+                       metres_per_unit=1000,
+                       unit_name='km',
+                       tol=0.01,
+                       angle=0,
+                       max_stripes=5,
+                       fontsize=8,
+                       dy=0.05)
