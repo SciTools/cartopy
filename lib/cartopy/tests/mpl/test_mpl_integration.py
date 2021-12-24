b940df03647ab581887a1297dc912e656fd6efda
Greg Lucas <greg.m.lucas@gmail.com>
2021-12-24 16:02:02 -0700

    TST: Update contour test limits
    Partial cherry-pick of 579be04b3a994d4e7c35e844db835068932a1597
    to increase the test tolerance of the images for MPL3.5 tests.

--- lib/cartopy/tests/mpl/test_mpl_integration.py
+++ lib/cartopy/tests/mpl/test_mpl_integration.py
@@ -19,7 +19,7 @@ from cartopy.tests.mpl import MPL_VERSION, ImageTesting
 
 # This is due to a change in MPL 3.5 contour line paths changing
 # ever so slightly.
-contour_tol = 2.24
+contour_tol = 2.25
 @pytest.mark.natural_earth
 @ImageTesting(['global_contour_wrap'], style='mpl20',
               tolerance=contour_tol)
