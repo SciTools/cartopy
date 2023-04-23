dd7a7d202fa0aed2b56265a1e86d2185ecc197e9
Ruth Comer <10599679+rcomer@users.noreply.github.com>
2023-04-23 10:23:13 +0100

    add py311 support

--- setup.py
+++ setup.py
@@ -249,6 +249,7 @@ setup(
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
             'Programming Language :: Python :: 3.10',
+            'Programming Language :: Python :: 3.11',
             'Programming Language :: Python :: 3 :: Only',
             'Topic :: Scientific/Engineering',
             'Topic :: Scientific/Engineering :: GIS',
