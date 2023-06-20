632201382ba1d642f4e183a249d90fa0a407e0d0
lgolston <30876419+lgolston@users.noreply.github.com>
2023-06-20 14:27:10 -0500

    create example

---
+++ examples/scalar_data/ocean_bathymetry.py
@@ -0,0 +1,88 @@
+"""
+Ocean bathymetry
+----------------
+
+Produces a map of ocean seafloor depth.
+
+"""
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import cartopy as cart
+import cartopy.crs as ccrs
+from glob import glob
+
+
+def load_bathymetry(zip_file_url):
+    """Read zip file from Natural Earth containing bathymetry shapefiles"""
+    # Download and extract shapefiles
+    import requests
+    import zipfile
+    import io
+    r = requests.get(zip_file_url)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall("ne_10m_bathymetry_all/")
+
+    # Read shapefiles, sorted by depth
+    shp_dict = {}
+    files = glob('ne_10m_bathymetry_all/*.shp')
+    assert len(files) > 0
+    files.sort()
+    depths = []
+    for f in files:
+        depth = '-' + f.split('_')[-1].split('.')[0]  # depth from file name
+        depths.append(depth)
+        bbox = (90, -15, 160, 60)  # (x0, y0, x1, y1)
+        nei = cart.io.shapereader.Reader(f, bbox=bbox)
+        shp_dict[depth] = nei
+    depths = np.array(depths)[::-1]  # sort from surface to bottom
+    return depths, shp_dict
+
+
+if __name__ == "__main__":
+    # Load data (14.8 MB file)
+    depths, shp_dict = load_bathymetry(
+        'https://naturalearth.s3.amazonaws.com/' +
+        '10m_physical/ne_10m_bathymetry_all.zip')
+
+    # Construct colormap:
+    # Depth levels in the colorbar are spaced as in the data
+    norm = matplotlib.colors.Normalize(vmin=-10000, vmax=0)  # in meters
+    depth_fraction = depths.astype(float) / depths.astype(float).min()
+    tmp = matplotlib.colormaps['Blues']  # to quantize: use .resampled(10)
+    colors_depths = tmp(depth_fraction)  # color values at fractional depths
+
+    # Set up plot
+    subplot_kw = {'projection': ccrs.LambertCylindrical()}
+    fig, ax = plt.subplots(subplot_kw=subplot_kw, figsize=(9, 7))
+    ax.set_extent([90, 160, -15, 60], crs=ccrs.PlateCarree())  # x0, x1, y0, y1
+
+    # Iterate and plot feature for each depth level
+    for i, depth in enumerate(depths):
+        ax.add_geometries(shp_dict[depth].geometries(),
+                          crs=ccrs.PlateCarree(),
+                          color=colors_depths[i])
+
+    # Add standard features
+    land_feature = cart.feature.NaturalEarthFeature(category='physical',
+                                                    name='LAND',
+                                                    scale='110m')
+    ax.add_feature(land_feature, color='grey')
+    ax.coastlines(lw=1, resolution='110m')
+    ax.gridlines(draw_labels=False)
+    ax.set_position([0.03, 0.05, 0.8, 0.9])
+
+    # Add custom colorbar
+    axi = fig.add_axes([0.85, 0.1, 0.025, 0.8])
+    ax.add_feature(cart.feature.BORDERS, linestyle=':')
+    matplotlib.colorbar.ColorbarBase(ax=axi,
+                                     cmap=tmp.reversed(),
+                                     norm=norm,
+                                     boundaries=depths.astype(int)[::-1],
+                                     spacing='proportional',
+                                     ticks=depths.astype(int),
+                                     label='Depth (m)')
+
+    # Convert vector bathymetries to raster (saves a lot of disk space)
+    # while leaving labels as vectors
+    ax.set_rasterized(True)
