d16768ecc8b87b788fb98b4585f61864b4b7566e
Greg Lucas <greg.m.lucas@gmail.com>
2023-10-07 09:35:42 -0600

    MNT: Change url for the Vesta image
    The current url was returning a 404. I looked for the same
    image name elsewhere on NASA and found this spot which looks
    reasonable.

--- examples/miscellanea/eccentric_ellipse.py
+++ examples/miscellanea/eccentric_ellipse.py
@@ -37,7 +37,7 @@ def vesta_image():
         the ``img_proj`` coordinate system.
 
     """
-    url = 'https://www.nasa.gov/sites/default/files/pia17037.jpg'
+    url = 'https://photojournal.jpl.nasa.gov/jpeg/PIA17037.jpg'
     img_handle = BytesIO(urlopen(url).read())
     raw_image = Image.open(img_handle)
     # The image is extremely high-resolution, which takes a long time to
