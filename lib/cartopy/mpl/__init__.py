a01a8263a9d13f858a2ccd382ad9364d90c36f9b
Ruth Comer <10599679+rcomer@users.noreply.github.com>
2023-10-16 10:07:57 +0100

    Remove redundant mpl version check

--- lib/cartopy/mpl/__init__.py
+++ lib/cartopy/mpl/__init__.py
@@ -9,8 +9,5 @@ import packaging.version
 
 
 _MPL_VERSION = packaging.version.parse(matplotlib.__version__)
-_MPL_35 = _MPL_VERSION.release[:2] >= (3, 5)
 _MPL_36 = _MPL_VERSION.release[:2] >= (3, 6)
 _MPL_38 = _MPL_VERSION.release[:2] >= (3, 8)
-
-assert _MPL_35, 'Cartopy is only supported with Matplotlib 3.5 or greater.'
