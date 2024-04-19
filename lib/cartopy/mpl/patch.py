5157129ec0ed44ae84134d299f17694f153e24de
Clarmy Lee <clarmylee92510@gmail.com>
2024-04-19 17:21:03 +0800

    fix: ImportError

--- lib/cartopy/mpl/patch.py
+++ lib/cartopy/mpl/patch.py
@@ -15,7 +15,12 @@ and `Matplotlib Path API <https://matplotlib.org/stable/api/path_api.html>`_.
 from matplotlib.path import Path
 import numpy as np
 import shapely.geometry as sgeom
-from shapely.errors import GEOSException
+import shapely
+
+if shapely.__version__ < '2.0.0':
+    from shapely.errors import TopologicalError as GEOSException
+else:
+    from shapely.errors import GEOSException
 
 def geos_to_path(shape):
     """
