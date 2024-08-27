aafe3f533aa59bd5844ac5a6111905aab076e50a
Thibault Hallouin <thibhlln@gmail.com>
2024-08-27 15:06:40 +0200

    add unit tests for L2E projection

--- lib/cartopy/tests/crs/test_lambert_conformal.py
+++ lib/cartopy/tests/crs/test_lambert_conformal.py
@@ -114,3 +114,29 @@ class Test_LambertConformal_standard_parallels:
         assert_array_almost_equal(n_pole_crs.y_limits,
                                   expected_y,
                                   decimal=0)
+
+
+class TestL2E:
+    def setup_class(self):
+        self.point_a = (48.13277955695077, 1.4868268900254693)
+        self.point_b = (48.68412929316207, -2.3188020040300126)
+        self.src_crs = ccrs.PlateCarree()
+        self.nan = float('nan')
+
+    def test_default(self):
+        proj = ccrs.L2E()
+        res = proj.transform_point(*self.point_a, src_crs=self.src_crs)
+        np.testing.assert_array_almost_equal(res,
+                                             (536745.89626, 2348522.77899),
+                                             decimal=5)
+        res = proj.transform_point(*self.point_b, src_crs=self.src_crs)
+        np.testing.assert_array_almost_equal(res,
+                                             (257266.90019, 2419663.00145),
+                                             decimal=5)
+
+    def test_nan(self):
+        proj = ccrs.L2E()
+        res = proj.transform_point(0.0, float('nan'), src_crs=self.src_crs)
+        assert np.all(np.isnan(res))
+        res = proj.transform_point(float('nan'), 0.0, src_crs=self.src_crs)
+        assert np.all(np.isnan(res))
