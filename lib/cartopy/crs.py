d550f653272a7052de944a8451b95d1a75adde08
AgentOxygen <cameronscummins@gmail.com>
2024-09-24 20:38:15 -0500

    Added winkel tripel projection class

--- lib/cartopy/crs.py
+++ lib/cartopy/crs.py
@@ -2493,6 +2493,33 @@ class Robinson(_WarpedRectangularProjection):
         return result
 
 
+class WinkelTripel(_WarpedRectangularProjection):
+    """
+    A Winkel-Tripel projection.
+
+    Compromise modified azimuthal projection that is less distorted
+    and more area-accurate. It is comparable to the Robinson
+    projection in both appearance and popularity.
+
+    The National Geographic Society uses the Winkel-Tripel projection
+    for most of the maps they produce.
+    """
+
+    def __init__(self, central_longitude=0.0, globe=None):
+        globe = globe or Globe(semimajor_axis=WGS84_SEMIMAJOR_AXIS)
+        proj4_params = [('proj', 'wintri'),
+                        ('lon_0', central_longitude),
+                        ('lat_0', 0.0)]
+
+        super(WinkelTripel, self).__init__(proj4_params,
+                                           central_longitude,
+                                           globe=globe)
+
+    @property
+    def threshold(self):
+        return 1e4
+
+
 class InterruptedGoodeHomolosine(Projection):
     """
     Composite equal-area projection emphasizing either land or
