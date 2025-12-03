d915cf4c33171f4e5f75efecd7a0ea38070cb75b
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2025-12-03 05:26:19 -0800

    bugfix: keep ruff happy

--- lib/cartopy/tests/mpl/test_images.py
+++ lib/cartopy/tests/mpl/test_images.py
@@ -3,6 +3,8 @@
 # This file is part of Cartopy and is released under the BSD 3-clause license.
 # See LICENSE in the root of the repository for full licensing details.
 
+from unittest import mock
+
 import types
 
 import matplotlib.colors as colors
