a0a2b3b5d99ad322c0851b9346df9e124750273d
Do Ngoc Khanh <14163153+epn09@users.noreply.github.com>
2026-09-01 09:54:19 +0900

    Update documentation for gamma parameter in crs.py

--- lib/cartopy/crs.py
+++ lib/cartopy/crs.py
@@ -3180,7 +3180,7 @@ class ObliqueMercator(Projection):
             the centre line. Defaults to 0.
         gamma: optional
             Azimuth of centerline clockwise from north of the rectified bearing
-            of centre line. If omitted, `alpha` determines the values of `gamma`.
+            of centre line. If omitted, `azimuth` determines the values of `gamma`.
         globe: optional
             An instance of :class:`cartopy.crs.Globe`. If omitted, a default
             globe is created.
